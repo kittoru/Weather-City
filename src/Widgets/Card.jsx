98c03a0eedf9c9d1f4947f9824552d2e247c4fc5
Kittoru <mama190774@gmail.com>
2024-09-22 16:39:53 +0300

    Fix Card

--- src/Widgets/Card.jsx
+++ src/Widgets/Card.jsx
@@ -3,12 +3,15 @@ import styles from './Card.module.scss'
 
 
 export const Card = () => {
+  const city = "Омск";
   return ( 
     <div className={styles.card}>
         <div className={styles.top}>
-          <Temperature temp={'+26'} subtemp={{min: '+20', max: '+26'}} description={'moderate rain'} type={'Rain'} tempFeels={'+24'}/>
+          <h1 className={styles.city}>{city}</h1>
+          <p>погода сегодня:</p>
         </div>
         <div className={styles.bottom}>
+          <Temperature temp={'+26'} subtemp={{min: '+20', max: '+26'}} description={'moderate rain'} type={'Rain'} tempFeels={'+24'}/>
           <Info speed={10} deg={'C'} humidity={60} pressure={778}/>
         </div>
     </div>
