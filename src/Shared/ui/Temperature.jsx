3bc21a56ebe3adf48318e9f5565ed555493072dc
Kittoru <mama190774@gmail.com>
2024-09-22 16:05:42 +0300

    Add Temperature and Card

--- src/Shared/ui/Temperature.jsx
+++ src/Shared/ui/Temperature.jsx
@@ -1,6 +1,6 @@
 import styles from './Temperature.module.scss';
 
-export const Temperature = ( { temp, icon, subtemp, type, tempFeels } ) => {
+export const Temperature = ( { temp, icon, subtemp, type, description, tempFeels } ) => {
   return ( 
     <div className={styles.temperature}>
       <div className={styles.main}>
@@ -10,16 +10,23 @@ export const Temperature = ( { temp, icon, subtemp, type, tempFeels } ) => {
         </div>
         <div className={styles.subtemp}>
           <div className={styles.min}>{subtemp.min}</div>
+          ...
           <div className={styles.max}>{subtemp.max}</div>
         </div>
       </div>
       <div className={styles.sub}>
-        <div className={styles.type}>
-          {type}
+        <div className={styles.group}>
+           <div className={styles.type}>
+            {type}
+          </div>
+          <div className={styles.description}>
+            {description}
+          </div>
         </div>
-        <div className={styles.description}>
+       
+        <div className={styles.feels}>
           Ощущается как  
-          <span className={styles.feels}>{tempFeels}</span>
+          <span className={styles.subFeels}> {tempFeels}</span>
         </div>
       </div>
     </div>
--- src/Widgets/Card.jsx
+++ src/Widgets/Card.jsx
@@ -5,8 +5,8 @@ import styles from './Card.module.scss'
 export const Card = () => {
   return ( 
     <div className={styles.card}>
-        <div className="top">
-          <Temperature temp={'+26'} subtemp={{min: '+20', max: '+26'}} type={'moderate rain'} tempFeels={'+24'}/>
+        <div className={styles.top}>
+          <Temperature temp={'+26'} subtemp={{min: '+20', max: '+26'}} description={'moderate rain'} type={'Rain'} tempFeels={'+24'}/>
         </div>
     </div>
    );
