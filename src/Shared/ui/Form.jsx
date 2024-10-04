a49e957ba6a1905963eed67260357d2b260beb4e
Kittoru <mama190774@gmail.com>
2024-10-04 19:13:51 +0300

    Fix svg search

--- src/Shared/ui/Form.jsx
+++ src/Shared/ui/Form.jsx
@@ -18,6 +18,9 @@ export const Form = () => {
     <form onSubmit={handelClick} className={styles.form}>
       <div className={styles.search}>
         <div className={styles.icon}>
+          <svg viewBox="0 0 24 24" fill="none">
+          <path d="M16.927 17.0401L20.4001 20.4001M19.2801 11.4401C19.2801 15.77 15.77 19.2801 11.4401 19.2801C7.11019 19.2801 3.6001 15.77 3.6001 11.4401C3.6001 7.11019 7.11019 3.6001 11.4401 3.6001C15.77 3.6001 19.2801 7.11019 19.2801 11.4401Z" stroke="black" stroke-width="2" stroke-linecap="round"/>
+          </svg>
           <Icon id="search"/>
         </div>
         <input type="text" value={value} placeholder='City...' className={styles.input} onInput={(e)=>{setValue(e.target.value)}}/>
